d62fc31f158221cdff545b32df5068804241171f
Brittany Keith <Keith@infoware.de>
2024-07-08 11:43:40 +0200

    Added example JSDoc comment in class

--- lib/core/Foo.ts
+++ lib/core/Foo.ts
@@ -1,6 +1,10 @@
 import { example } from "../types/example.js"; //note that all imports should point to .js files! This is because TS will output a .js file once it is transpiled using tsc
 import { bar } from "./Bar.js"
 
+/**
+ * An example of a JSDoc comment which will show up to the user when they install the package
+ */
+
 class Foo {
     private _property: example; //any properties in the constructor must have a type declared before the constructor
     bar: typeof bar; //if you import a class or property, TS can infer it's types using typeof 
@@ -12,12 +16,19 @@ class Foo {
     private someMethod(){
         return this._property
     }
-    //this will be public and anyone who imports your package will see the method 
-    public someOtherMethod(param: string, otherParam: number){
+      /**
+   * A method to return the toll rate based on the CO2 Class, Axle & Weight Class, and Euro Emission Class of the truck. 
+   * @param {string} param - some param
+   * @param {number} otherParam - some other param that is a number
+   * @returns {number} - returns a random number between 0 to <1
+   */
+
+    public someOtherMethod(param: string, otherParam: number):number{
         for(let i = 0; i <= otherParam; i++){
             console.log(this._property.foo + '' + param ) 
             console.log(this.bar.three)
         }
+        return Math.random()
       
     }
 }
